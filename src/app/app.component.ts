394c31438379643e33892cc2ec7f10cd88652628
jimwell <jimwelllmanalo@gmail.com>
2025-02-09 23:53:05 +0800

    Set up Angular project with routing, navbar, and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  RouterOutlet,
+  RouterLink,
+  Router,
+  RouterModule,
+} from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio-manalo-jimwell';
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 5) {
+      console.log(window.scrollY);
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  mainHeading: string = 'Jimwell Manalo';
+  secondaryHeading: string = 'Backend Developer';
+  tagline: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
+}
