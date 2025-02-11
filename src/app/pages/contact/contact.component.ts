30659c59f51cd5befe525bd3c4bac5837f8a29eb
jimwell <jimwelllmanalo@gmail.com>
2025-02-12 02:45:29 +0800

    Create project-view component to display specific project details

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
